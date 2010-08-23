4b33b4cb3fc29d1f8251afa68e0d1be5e8237511
Gregory Brown <gregory.t.brown@gmail.com>
2010-08-23 17:13:43 -0400

    Basic board features:  Prevent stones from being played atop one another, allow stones to be removed, etc

--- lib/go.rb
+++ lib/go.rb
@@ -1,13 +1,31 @@
 module Go
   class Board
+
+    VALID_MARKERS = [:black, :white, nil]
+
+    InvalidStoneTypeError        = Class.new(StandardError)
+    AlreadyOccupiedPositionError = Class.new(StandardError)
+
     def initialize(board_size)
       @size = board_size
+      @data = size.times.map { Array.new(size) }
     end
 
-    attr_reader :size
+    attr_reader :size, :data
 
     def dimensions
       "#{size}x#{size}"
+    end
+
+    def [](x,y)
+      @data[x][y]
+    end
+
+    def []=(x,y,marker)
+      raise InvalidStoneTypeError unless VALID_MARKERS.include?(marker)
+      raise AlreadyOccupiedPositionError if @data[x][y] && marker
+
+      @data[x][y] = marker
     end
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,8 +1,69 @@
 require_relative "test_helper"
 
 class BoardTest < Test::Unit::TestCase
-  test "A board has dimensions" do
-    board = Go::Board.new(9)
-    assert_equal "9x9", board.dimensions
+
+  setup do
+    @board = Go::Board.new(9)
   end
+
+  test "A board has dimensions" do
+    assert_equal "9x9", @board.dimensions
+  end
+
+  test "All intersections are empty by default" do
+    @board.size.times do |x|
+      @board.size.times do |y|
+        assert_nil @board[x,y]
+      end
+    end
+  end
+
+  test "Can place a white stone on the board" do
+    @board[3,4] = :white
+    assert_equal :white, @board[3,4]
+  end
+
+  test "Can place a black stone on the board" do
+    @board[3,5] = :black
+    assert_equal :black, @board[3,5]
+  end
+
+  test "Can empty an intersection" do
+    @board[3,4] = :black
+    @board[3,4] = nil
+    assert_equal nil, @board[3,4]
+  end
+
+  test "Can place a new stone on an emptied intersection" do
+    @board[3,4] = :black
+
+    @board[3,4] = nil
+    @board[3,4] = :white
+    assert_equal :white, @board[3,4]
+
+    @board[3,4] = nil
+    @board[3,4] = :black
+    assert_equal :black, @board[3,4]
+  end
+
+  test "Cannot replace a black stone with a white stone" do
+    @board[3,4] = :black
+    assert_raises(Go::Board::AlreadyOccupiedPositionError) do
+      @board[3,4] = :white
+    end
+  end
+
+  test "Cannot replace a white stone with a black stone" do
+    @board[3,4] = :white
+    assert_raises(Go::Board::AlreadyOccupiedPositionError) do
+      @board[3,4] = :black
+    end
+  end
+
+  test "Cannot place arbitrary markers on board" do
+    assert_raises(Go::Board::InvalidStoneTypeError) do
+      @board[3,6] = :green
+    end
+  end
+
 end
